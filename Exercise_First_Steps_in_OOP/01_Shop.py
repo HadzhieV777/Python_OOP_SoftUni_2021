a2f47f017f98947627ffddbf987e4bd439604d18
Hadzhiev <bdwhadjiev42@gmail.com>
2021-10-26 09:30:07 +0300

    Uploaded the problem 01_Shop from Exercise First Steps in OOP

---
+++ Exercise_First_Steps_in_OOP/01_Shop.py
@@ -0,0 +1,15 @@
+# # 01. Shop
+
+class Shop:
+    def __init__(self, name: str, items: list):
+        self.name = name
+        self.items = items
+
+    def get_items_count(self):
+        return len(self.items)
+
+shop = Shop("My Shop", ["Apples", "Bananas", "Cucumbers"])
+print(shop.get_items_count())
+
+shop = Shop("My Shop", ["Apples", "Bananas", "Cucumbers"])
+print(shop.get_items_count())
