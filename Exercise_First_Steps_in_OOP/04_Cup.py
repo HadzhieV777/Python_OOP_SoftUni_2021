c80fe64982b30289c1a8de8e638bbf7d8ab68724
Hadzhiev <bdwhadjiev42@gmail.com>
2021-10-27 08:32:47 +0300

    Uploaded the problem 04_Cup from Exercise First Steps in OOP

---
+++ Exercise_First_Steps_in_OOP/04_Cup.py
@@ -0,0 +1,22 @@
+# 04. Cup
+
+class Cup:
+    def __init__(self, size: int, quantity: int):
+        self.size = size
+        self.quantity = quantity
+
+    def fill(self, quantity):
+        free_space = (self.size - self.quantity)
+        if quantity <= free_space:
+            self.quantity += quantity
+
+    def status(self):
+        free_space = (self.size - self.quantity)
+        return free_space
+
+
+cup = Cup(100, 50)
+print(cup.status())
+cup.fill(40)
+cup.fill(20)
+print(cup.status())
