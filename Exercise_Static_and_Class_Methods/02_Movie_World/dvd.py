9e88276c13eed62c6fbbfbdc40ebe5fc3c0b4a2a
Hadzhiev <bdwhadjiev42@gmail.com>
2021-11-12 11:07:15 +0200

    Uploaded the dvd class file which is part of problem 02_Movie_World from Exercise_Static_and_Class_Methods

---
+++ Exercise_Static_and_Class_Methods/02_Movie_World/dvd.py
@@ -0,0 +1,26 @@
+from project.months_mapper import mapper
+
+
+class DVD:
+    def __init__(self, name: str, id: int, creation_year: int, creation_month: str, age_restriction: int):
+        self.name = name
+        self.id = id
+        self.creation_year = creation_year
+        self.creation_month = creation_month
+        self.age_restriction = age_restriction
+        self.is_rented = False
+
+    @classmethod
+    def from_date(cls, id: int, name: str, date: str, age_restriction: int):
+        day, month, year = [int(x) for x in date.split('.')]
+        month_name = mapper[month]
+        return cls(name, id, year, month_name, age_restriction)
+
+    def __repr__(self):
+        status = 'rented' if self.is_rented else 'not rented'
+        return f"{self.id}: {self.name} ({self.creation_month} {self.creation_year})" \
+               f" has age restriction {self.age_restriction}. Status: {status}"
+
+
+
+
