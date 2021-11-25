33327923f43c23ca610dfaf7b806e228f3779914
Hadzhiev <bdwhadjiev42@gmail.com>
2021-11-25 11:26:55 +0200

    Uploaded problem # 05_Take_Halves from Exercise_Iterators_and_Generators

---
+++ Exercise_Iterators_and_Generators/05_Take_Halves.py
@@ -0,0 +1,29 @@
+# 05. Take Halves
+
+def solution():
+    def integers():
+        num = 1
+        while True:
+            yield num
+            num += 1
+
+    def halves():
+        for i in integers():
+            yield i / 2
+
+    def take(n, seq):
+        result = []
+        for _ in range(n):
+            result.append(next(seq))
+        return result
+
+    return (take, halves, integers)
+
+
+take = solution()[0]
+halves = solution()[1]
+print(take(5, halves()))
+
+take = solution()[0]
+halves = solution()[1]
+print(take(0, halves()))
