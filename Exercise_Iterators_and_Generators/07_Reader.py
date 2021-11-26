491de212ee88bad108837a6e5b3efee76b5083f0
Hadzhiev <bdwhadjiev42@gmail.com>
2021-11-26 14:46:01 +0200

    Uploaded problem # 07_Reader from Exercise_Iterators_and_Generators

---
+++ Exercise_Iterators_and_Generators/07_Reader.py
@@ -0,0 +1,14 @@
+# 07. Reader
+
+def read_next(*args):
+    for iterable in args:
+        for el in iterable:
+            yield el
+
+
+for item in read_next("string", (2,), {"d": 1, "I": 2, "c": 3, "t": 4}):
+    print(item, end='')
+
+
+for i in read_next("Need", (2, 3), ["words", "."]):
+    print(i)
