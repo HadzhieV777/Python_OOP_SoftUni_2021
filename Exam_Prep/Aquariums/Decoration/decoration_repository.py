09c4a738d8c64c60a78d89a293959092e3778371
Hadzhiev <bdwhadjiev42@gmail.com>
2021-12-10 09:08:31 +0200

    Uploaded the decoration_repository file from the decoration directory from the Exam - 10 April 2021

---
+++ Exam_Prep/Aquariums/Decoration/decoration_repository.py
@@ -0,0 +1,19 @@
+class DecorationRepository:
+    def __init__(self):
+        self.decorations = []
+
+    def add(self, decoration):
+        self.decorations.append(decoration)
+
+    def remove(self, decoration):
+        if decoration in self.decorations:
+            self.decorations.remove(decoration)
+            return True
+        return False
+
+    def find_by_type(self, decoration_type):
+        for decoration in self.decorations:
+            if type(decoration).__name__ == decoration_type:
+                return decoration
+        return 'None'
+
