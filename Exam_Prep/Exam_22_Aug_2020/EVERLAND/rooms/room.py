23cafd14f3b2db6bef53784e4125eed9cc490762
Rado <bdwhadjiev42@gmail.com>
2021-12-25 09:01:47 +0200

    Uploaded the room file from rooms directory from Exam_22_Aug_2020/EVERLAND

---
+++ Exam_Prep/Exam_22_Aug_2020/EVERLAND/rooms/room.py
@@ -0,0 +1,50 @@
+class Room:
+    __DEFAULT_ROOM_COST = 0
+    __DEFAULT_CHILDREN: list = []
+    __DEFAULT_APPLIANCES: list = []
+    __DEFAULT_EXPENSES: float = 0.0
+
+    def __init__(self, name: str, budget: float, members_count: int) -> None:
+        self.family_name = name
+        self.budget = budget
+        self.members_count = members_count
+        self.room_cost = self.__DEFAULT_ROOM_COST
+        self.children = Room.__DEFAULT_CHILDREN
+        self.appliances = Room.__DEFAULT_APPLIANCES
+        self.expenses = Room.__DEFAULT_EXPENSES
+
+    @property
+    def expenses(self):
+        return self.__expenses
+
+    @expenses.setter
+    def expenses(self, value):
+        if value < 0:
+            raise ValueError(f"Expenses cannot be negative")
+
+        self.__expenses = value
+
+    @property
+    def total_cost(self):
+        return self.expenses + self.room_cost
+
+    def calculate_expenses(self, *args) -> None:
+        for list_data in args:
+            for obj in list_data:
+                self.expenses += obj.get_monthly_expense()
+
+    def can_pay(self):
+        return self.budget >= self.total_cost
+
+    def __repr__(self) -> str:
+        info = [
+            f"{self.family_name} with {self.members_count} members. ",
+            f"Budget: {self.budget:.2f}$, Expenses: {self.expenses:.2f}$",
+        ]
+
+        for i, child in enumerate(self.children):
+            info.append(f"--- Child {i + 1} monthly cost: {child.get_monthly_expense():.2f}$")
+
+        appliance_monthly_expenses = sum([appliance.get_monthly_expense() for appliance in self.appliances])
+        info.append(f"--- Appliances monthly cost: {appliance_monthly_expenses:.2f}$")
+        return '\n'.join(info)
