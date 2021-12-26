07ce5fb370af430da89b574248a7044817237107
Rado <bdwhadjiev42@gmail.com>
2021-12-26 09:11:18 +0200

    Uploaded the everland file from main directory from Exam_22_Aug_2020/EVERLAND

---
+++ Exam_Prep/Exam_22_Aug_2020/EVERLAND/everland.py
@@ -0,0 +1,33 @@
+from project.rooms.room import Room
+
+
+class Everland:
+    def __init__(self):
+        self.rooms = []
+
+    def add_room(self, room: Room):
+        self.rooms.append(room)
+
+    def get_monthly_consumptions(self):
+        expenses = sum([room.calculate_expenses() for room in self.rooms])
+        return f"Monthly consumption: {expenses}$."
+
+    def pay(self) -> str:
+        info = []
+        for room in self.rooms:
+            if room.can_pay():
+                room.budget -= room.total_cost
+                info.append(f"{room.family_name} paid {room.total_cost:.2f}$ and have {room.budget:.2f}$ left.")
+            else:
+                self.rooms.remove(room)
+                info.append(f"{room.family_name} does not have enough budget and must leave the hotel.")
+
+        return '\n'.join(info)
+
+    def status(self):
+        info = [repr(room) for room in self.rooms]
+        info.insert(0, f"Total population: {self._get_total_population()}")
+        return '\n'.join(info)
+
+    def _get_total_population(self):
+        return sum([room.members_count for room in self.rooms])
