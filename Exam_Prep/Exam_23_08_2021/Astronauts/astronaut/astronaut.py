49fd263617e8083f14e7be961d8645a7401f6c84
Rado <bdwhadjiev42@gmail.com>
2021-12-27 09:15:49 +0200

    Uploaded the astronaut file from astronaut directory from Exam_Prep/Exam_23_08_2021/Astronauts

---
+++ Exam_Prep/Exam_23_08_2021/Astronauts/astronaut/astronaut.py
@@ -0,0 +1,32 @@
+from abc import ABC, abstractmethod
+
+
+class Astronaut(ABC):
+    # @abstractmethod
+    def __init__(self, name, oxygen):
+        self.name = name
+        self.oxygen = oxygen
+        self.backpack = []
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, value):
+        if value.strip() == '':
+            raise ValueError("Astronaut name cannot be empty string or whitespace!")
+        self.__name = value
+
+    def breathe(self):
+        self.oxygen -= 10
+
+    def increase_oxygen(self, amount):
+        self.oxygen += amount
+
+    def __str__(self):
+        result = f'Name: {self.name}' + '\n'
+        result += f'Oxygen: {self.oxygen}' + '\n'
+        result += f"Backpack items: {', '.join(self.backpack) if len(self.backpack) > 0 else 'none'}"
+
+        return result
